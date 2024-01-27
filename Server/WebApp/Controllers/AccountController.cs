c22cb84d04482baf133b4e6def0ee0a9e7929825
stanislau-by <webel14ras@gmail.com>
2024-01-27 13:46:20 +0200

    create empty account controller

---
+++ Server/WebApp/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpPost]
+        public Task<string> Authorization()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public Task Registration()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public Task ResetPassword()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public Task RestorePassword()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
